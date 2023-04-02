2f090818eb71d4b9c7d7468c5b05080d9bd225cf
00014511 <00014511.wiutstudent@gmail.com>
2023-04-03 02:03:09 +0500

    categories

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express")
+const path = require("path")
+
 const app = express()
 const PORT = 3000
 
+app.set("view engine", "pug");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, "public")))
 
-app.listen(PORT,() => {
+app.get("/", (req, res) => {
+    res.render("spendings/spendings.pug")
+})
+
+const { categoryRoutes } = require("./routes/categories.js")
+
+app.use("/categories", categoryRoutes)
+
+app.listen(PORT, () => {
     console.log("App is running on port " + PORT);
 })
